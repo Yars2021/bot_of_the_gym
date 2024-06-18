96dead6724cd40b4ae538a2be7925f57055eb6bf
Yars2021 <yaroslav.sukhovey@yandex.ru>
2024-06-18 19:52:48 +0300

    Added notifications module

--- bot.py
+++ bot.py
@@ -1,3 +1,5 @@
+import datetime
+
 import discord
 from discord import app_commands
 from discord.ext import tasks
@@ -5,6 +7,7 @@ from discord.ext import tasks
 import utils
 from AudioModule import AudioModule
 from AudioModule import Soundboard
+from NotificationsModule import NotificationsModule
 from PictureModule import PictureModule
 from SizeModule import SizeModule
 
@@ -30,7 +33,8 @@ MESSAGE_TIMER = {
 MODULES = {
     "size_mod": SizeModule(),
     "picture_mod": PictureModule(),
-    "audio_mod": AudioModule()
+    "audio_mod": AudioModule(),
+    "notifications_mod": NotificationsModule()
 }
 
 
@@ -40,6 +44,7 @@ MODULES = {
 
 
 intents = discord.Intents.default()
+intents.dm_messages = True
 client = discord.Client(intents=intents)
 command_tree = app_commands.CommandTree(client)
 
@@ -452,10 +457,131 @@ async def command_reset_avatar(interaction: discord.Interaction, resolution: str
 
 
 @tasks.loop(hours=24)
-async def profile_pic_loop():
+async def profile_pic_routine():
     global MODULES
 
     await client.user.edit(avatar=await MODULES["picture_mod"].random_profile_pic())
+
+
+########################################################################################################################
+#                                            Команды notifications_mod
+########################################################################################################################
+
+
+@command_tree.command(
+    name="clear_notification_history",
+    description="Очистить историю нотификаций",
+    guild=discord.Object(id=SERVER_ID)
+)
+async def command_clear_notification_history(interaction: discord.Interaction) -> None:
+    global ADMIN_USERNAMES
+    global MESSAGE_TIMER
+    global MODULES
+
+    if await utils.check_permissions(interaction, ADMIN_USERNAMES) and \
+            await utils.check_module(interaction, MODULES["notifications_mod"].MODULE, MODULES):
+        await interaction.response.send_message("История очищена", ephemeral=True, delete_after=MESSAGE_TIMER["normal"])
+        MODULES["notifications_mod"].clear_history()
+
+
+@command_tree.command(
+    name="force_clear_files",
+    description="Принудительно удалить все старые файлы нотификаций",
+    guild=discord.Object(id=SERVER_ID)
+)
+async def command_force_clear_files(interaction: discord.Interaction) -> None:
+    global ADMIN_USERNAMES
+    global MESSAGE_TIMER
+    global MODULES
+
+    if await utils.check_permissions(interaction, ADMIN_USERNAMES) and \
+            await utils.check_module(interaction, MODULES["notifications_mod"].MODULE, MODULES):
+        await interaction.response.send_message("Старые файлы удалены", ephemeral=True, delete_after=MESSAGE_TIMER["normal"])
+        MODULES["notifications_mod"].clear_old()
+
+
+@command_tree.command(
+    name="get_notification_history",
+    description="Получить историю нотификаций",
+    guild=discord.Object(id=SERVER_ID)
+)
+async def command_get_notification_history(interaction: discord.Interaction) -> None:
+    global ADMIN_USERNAMES
+    global MESSAGE_TIMER
+    global MODULES
+
+    if await utils.check_permissions(interaction, ADMIN_USERNAMES) and \
+            await utils.check_module(interaction, MODULES["notifications_mod"].MODULE, MODULES):
+        await interaction.response.send_message(MODULES["notifications_mod"].get_history(),
+                                                ephemeral=True,
+                                                delete_after=MESSAGE_TIMER["normal"])
+
+
+@client.event
+async def on_message(message):
+    global MODULES
+
+    if message.author != client.user and isinstance(message.channel, discord.DMChannel):
+        if message.content == "/help":
+            await message.channel.send("Команды:\n/help - выводит список доступных команд\n" +
+                                       "/notify - создает напоминание на указанную дату и время.\n" +
+                                       "\tИспользование: /notify <TZ> <DATE> <TIME> \"<TITLE>\" \"<TEXT>\"\n" +
+                                       "\t\t<TZ> - часовой сдвиг относительно времени UTC, число от -24 до 24\n" +
+                                       "\t\t<DATE> - дата в формате год-месяц-день (2024-06-18)\n" +
+                                       "\t\t<TIME> - время в формате часы:минуты:секунды (19:11:42)\n" +
+                                       "\t\t<TITLE> - звголовок напоминания, то, что отобразится в первую очередь\n" +
+                                       "\t\t<TEXT> - текст напоминания\n" +
+                                       "\t\tВАЖНО! - заголовк и текст указываются в двойных кавычках (\").\n" +
+                                       "\t\tЭто позволяет использовать внутри них пробелы\n"
+                                       "\tПример:\n" +
+                                       "\t\t/notify 3 2024-06-18 19:43:50 \"Тестовый Заголовок\" \"Текст Напоминания\"")
+        elif message.content.startswith("/notify"):
+            args = message.content.split()
+
+            try:
+                timezone = int(args[1])
+                if timezone < -24 or timezone > 24:
+                    await message.channel.send("Неверный формат часового сдвига. Ожидается число от -24 до 24")
+
+                complex_args = message.content.split("\"")
+
+                MODULES["notifications_mod"].queue_notification(message.author.id,
+                                                                timezone,
+                                                                args[2],
+                                                                args[3],
+                                                                complex_args[1],
+                                                                complex_args[3])
+
+                MODULES["notifications_mod"].save_notification()
+
+                await message.channel.send("Напоминание создано")
+            except Exception:
+                await message.channel.send("Неверный формат аргументов.\nИспользуйте /help для отображения справки")
+
+        else:
+            await message.channel.send("Неизвестная команда.\nИспользуйте /help для отображения списка команд")
+
+
+@tasks.loop(seconds=0.5)
+async def notifications_send_routine():
+    global MODULES
+
+    MODULES["notifications_mod"].save_notification()
+
+    notifications_to_send = MODULES["notifications_mod"].notify_tick()
+
+    for notification in notifications_to_send:
+        author = await client.fetch_user(notification["author"])
+
+        await author.send("**" + notification["title"] + "**")
+        await author.send(notification["text"])
+
+
+@tasks.loop(hours=24)
+async def notifications_cleanup_routine():
+    global MODULES
+
+    MODULES["notifications_mod"].clear_old()
 
 
 ########################################################################################################################
@@ -468,7 +594,9 @@ async def on_ready():
     global command_tree
 
     await command_tree.sync(guild=discord.Object(id=SERVER_ID))
-    profile_pic_loop.start()
+    profile_pic_routine.start()
+    notifications_send_routine.start()
+    notifications_cleanup_routine.start()
 
     print("Bot is up!")
 
